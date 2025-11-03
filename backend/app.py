7058d36e85d0392ca25295bb4789b19fa8316f88
erfanalizada <erfanalizada6@gmail.com>
2025-11-03 19:19:48 +0100

    app.py yser changed

--- backend/app.py
+++ backend/app.py
@@ -2,15 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/api/users/test-db", methods=["POST"])
-def test_db():
-    try:
-        with engine.connect() as conn:
-            result = conn.execute(text("SELECT NOW()")).fetchone()
-            return {"message": "Database OK", "time": str(result[0])}, 200
-    except Exception as e:
-        return {"error": str(e)}, 500
-
 
 @app.route("/api/hello")
 def hello():
--- backend/user-service/app.py
+++ backend/user-service/app.py
@@ -14,6 +14,15 @@ def profile():
 def health():
     return {"status": "user-service ok"}
 
+@app.route("/api/users/test-db", methods=["POST"])
+def test_db():
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(text("SELECT NOW()")).fetchone()
+            return {"message": "Database OK", "time": str(result[0])}, 200
+    except Exception as e:
+        return {"error": str(e)}, 500
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
 
